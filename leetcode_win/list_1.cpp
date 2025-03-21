c00abc121240ebed3521ff49cc6b6b455b7b4830
chuiyugin <1726123207@qq.com>
2025-03-21 19:26:12 +0800

    update

--- leetcode_win/list_1.cpp
+++ leetcode_win/list_1.cpp
@@ -25,16 +25,85 @@ struct ListNode
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+//解决方案
+class Solution {
+    public:
+        // ListNode* removeElements(ListNode* head, int val) {
+        //     ListNode *now = head;
+        //     ListNode *pre = nullptr;//now的前一个
+        //     ListNode *temp = nullptr;
+        //     while(now != nullptr)
+        //     {
+        //         //头节点就是要删除的数据
+        //         if(head->val == val)
+        //         {
+        //             head = head->next;
+        //             delete now;
+        //             now = head;
+        //         }
+        //         //头节点不是要删除的数据
+        //         else if(now->val == val && head->val != val)
+        //         {
+        //             pre->next = now->next;
+        //             temp = now;
+        //             now = now->next;
+        //             delete temp;
+        //             temp = nullptr;
+        //         }
+        //         //没有要删除的数据
+        //         else
+        //         {
+        //             pre = now;
+        //             now = now->next;
+        //         }
+        //     }
+        //     return head;
+        // }
+
+        //虚拟头节点解法
+        ListNode* removeElements(ListNode* head, int val) {
+            ListNode* vitHead = new ListNode(-1);
+            vitHead->next = head;
+            ListNode* now = vitHead;
+            while(now->next != nullptr)
+            {
+                if(now->next->val == val)
+                {
+                    ListNode* temp = now->next; 
+                    now->next = now->next->next;
+                    now = now->next;
+                    delete temp;
+                }
+                else
+                {
+                    now = now->next;
+                }
+            }
+            return vitHead->next;
+        }
+    };
+
 int main()
 {
     ListNode *head = new ListNode(1);
     ListNode *now = head;
-    ListNode *next = head->next;
     for(int i=2;i<=8;i++)
     {
-        
+        ListNode *temp = new ListNode(i);
+        now->next = temp;
+        now = now->next;
     }
-    printf("%d\n",now->val);
+    now = head;
+    
+    Solution s;
+    now = s.removeElements(head,1);
+
+    while(now->next!=nullptr)
+    {
+        printf("%d\n",now->val);
+        now = now->next;
+    }
+    
     system("pause"); // 防止运行后自动退出，需头文件stdlib.h
     return 0;
 }
