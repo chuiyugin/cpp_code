eb38d99c2cad7a46c455967dbf12cbb2cd87ef71
chuiyugin <1726123207@qq.com>
2025-05-07 21:23:14 +0800

    update

--- prj/prj_bst/main.c
+++ prj/prj_bst/main.c
@@ -10,12 +10,26 @@ int main(void)
     bst_insert(tree,57);
     bst_insert(tree,3);
 
-    TreeNode* node1 = bst_search(tree,42);
+    TreeNode* node1 = bst_search(tree,9);
     printf("key: %d\n",node1->key);
     printf("left: %d\n",node1->left->key);
     printf("right: %d\n",node1->right->key);
 
+    //广度搜索
     bst_levellorder(tree);
+    printf("\n");
+    bst_delete(tree,9);
+    bst_levellorder(tree);
+
+    //深度搜索
+    // bst_inorder(tree->root);
+    // printf("\n");
+    // bst_preorder(tree->root);
+    // printf("\n");
+    // bst_backorder(tree->root);
+
+    bst_destroy(tree->root);
+    free(tree);
 
     return 0;
 }
